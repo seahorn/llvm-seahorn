e97ceb2e38eb72014d7058e298cb5406074ffb2f
Jared Carlson <jcarlson@gototheboard.com>
2017-06-02 21:47:52 -0400

    Moving into Loops, not sure I can just immediately copy, will try though...

--- lib/Transforms/InstNondet/NondetInit.cc
+++ lib/Transforms/InstNondet/NondetInit.cc
@@ -24,7 +24,7 @@ DM-0002198
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Support/Compiler.h"
-#include "llvm/PassManager.h"
+#include "llvm/IR/PassManager.h"
 
 #include "llvm/IR/IRBuilder.h"
 
