480665259d8697f63b7b6f292a63878901edb7b6
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-04-16 09:12:19 -0400

    Move our code out of the way

--- tools/opt/AnalysisWrappers.cpp
+++ tools/opt/AnalysisWrappers.cpp
@@ -72,22 +72,3 @@ static RegisterPass<ExternalFunctionsPassedConstants>
   P1("print-externalfnconstants",
      "Print external fn callsites passed constants");
 
-namespace {
-  struct CallGraphPrinter : public ModulePass {
-    static char ID; // Pass ID, replacement for typeid
-    CallGraphPrinter() : ModulePass(ID) {}
-
-    void getAnalysisUsage(AnalysisUsage &AU) const override {
-      AU.setPreservesAll();
-      AU.addRequiredTransitive<CallGraphWrapperPass>();
-    }
-    bool runOnModule(Module &M) override {
-      getAnalysis<CallGraphWrapperPass>().print(errs(), &M);
-      return false;
-    }
-  };
-}
-
-char CallGraphPrinter::ID = 0;
-static RegisterPass<CallGraphPrinter>
-  P2("sea-print-callgraph", "Print a call graph");
