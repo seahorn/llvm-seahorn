884619010e94e830ec504cce68af6366fdeda574
Siddharth Priya <s2priya@uwaterloo.ca>
2026-06-26 21:11:53 -0400

    [*] import from llvm17
    Pristine LLVM 17 upstream source for the InstCombine/Loops/IPO/opt files,
    imported on top of dev16 so the dev17 baseline carries dev16's project config
    (CMAKE_CXX_STANDARD 17, scaffolding) rather than the dev14-era import chain.
    LLVM 17 removed PassManagerBuilder.cpp. SeaHorn customizations layer on top.

--- lib/Transforms/InstCombine/InstCombineAtomicRMW.cpp
+++ lib/Transforms/InstCombine/InstCombineAtomicRMW.cpp
@@ -14,7 +14,6 @@
 #include "llvm/IR/Instructions.h"
 
 using namespace llvm;
-using namespace llvm_seahorn;
 
 namespace {
 /// Return true if and only if the given instruction does not modify the memory
@@ -101,7 +100,7 @@ bool isSaturating(AtomicRMWInst& RMWI) {
 }
 } // namespace
 
-Instruction *SeaInstCombinerImpl::visitAtomicRMWInst(AtomicRMWInst &RMWI) {
+Instruction *InstCombinerImpl::visitAtomicRMWInst(AtomicRMWInst &RMWI) {
 
   // Volatile RMWs perform a load and a store, we cannot replace this by just a
   // load or just a store. We chose not to canonicalize out of general paranoia
@@ -117,23 +116,9 @@ Instruction *SeaInstCombinerImpl::visitAtomicRMWInst(AtomicRMWInst &RMWI) {
     return &RMWI;
   }
 
-  AtomicOrdering Ordering = RMWI.getOrdering();
-  assert(Ordering != AtomicOrdering::NotAtomic &&
-         Ordering != AtomicOrdering::Unordered &&
+  assert(RMWI.getOrdering() != AtomicOrdering::NotAtomic &&
+         RMWI.getOrdering() != AtomicOrdering::Unordered &&
          "AtomicRMWs don't make sense with Unordered or NotAtomic");
-
-  // Any atomicrmw xchg with no uses can be converted to a atomic store if the
-  // ordering is compatible.
-  if (RMWI.getOperation() == AtomicRMWInst::Xchg &&
-      RMWI.use_empty()) {
-    if (Ordering != AtomicOrdering::Release &&
-        Ordering != AtomicOrdering::Monotonic)
-      return nullptr;
-    new StoreInst(RMWI.getValOperand(), RMWI.getPointerOperand(),
-                  /*isVolatile*/ false, RMWI.getAlign(), Ordering,
-                  RMWI.getSyncScopeID(), &RMWI);
-    return eraseInstFromFunction(RMWI);
-  }
 
   if (!isIdempotentRMW(RMWI))
     return nullptr;
